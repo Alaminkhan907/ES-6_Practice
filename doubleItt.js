6c9dcc93e0d24c2b1ebd15fa7f68d885e556465c
mdkhaz <mdkhaz@ttu.ee>
2021-02-08 00:46:23 +0200

    fistcommit

---
+++ doubleItt.js
@@ -0,0 +1,5 @@
+function doubleIT(num){
+    return num*2;
+}
+const result = doubleIT(5);
+console.log(result);
---
+++ letconst.js
@@ -0,0 +1,19 @@
+const hubby ='alamin khan';
+//hubby ='alo khan';
+console.log(hubby);
+
+/*const nums = [12,45];
+nums[1]= 88;
+nums.push(12);
+console.log(nums);*/
+
+let patientName = 'rahim';
+patientName = 'jasim uddin';
+console.log(patientName);
+
+let sum = 0;
+for (var i = 0; i < 10; i++) {
+    sum = sum + i;
+    
+}
+console.log(i);
